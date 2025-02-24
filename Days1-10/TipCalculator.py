280af8e3382ff2552c8b2de7834574eca9e2d2bb
Dmand723 <dawson.simmons723@gmail.com>
2025-02-24 10:50:04 -0700

    Removed 1-10 on 11-20 branch

--- Days1-10/TipCalculator.py
+++
@@ -1,8 +0,0 @@
-bill = float(input("What is the total of the bill?: $"))
-percent = int(input("What percent of tip would you like to give? % "))
-totalPeople = int(input("How many people are you spliting the bill between?: "))
-percent = percent/100
-tipAmount = bill * percent
-totalBill = bill+tipAmount
-billPerPerson = round((totalBill/totalPeople),2)
-print(f"Each person should pay ${billPerPerson}")
--- Days1-10/bandNameGen.py
+++
@@ -1,4 +0,0 @@
-print('Welcome to the band name genorator')
-city = input('What city did you grow up in?: ')
-petName = input("What is a name of one of your pets?: ")
-print(f'Your band name could be {city} {petName}')
